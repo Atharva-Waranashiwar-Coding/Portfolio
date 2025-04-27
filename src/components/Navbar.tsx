6d7253a8f9fb4f9a0cd2e02f7a5ca6cb7d591b2f
Atharva Waranashiwar <waranashiwaratharva@gmail.com>
2025-04-26 23:01:01 -0400

    Improved css for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,23 +12,26 @@ function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="bg-[#CA0000] shadow-md">
-      <div className="flex h-20">
+    <nav className="bg-gradient-to-r from-[#b00000] to-[#CA0000] shadow-md">
+      <div className="flex h-20 items-center">
 
         {/* Red Section only for the Name */}
-        <div className="flex items-center bg-[#100001] px-8 h-full clip-diagonal-right mr-8">
-          <Link to="/" className="text-white text-2xl font-bold tracking-wide whitespace-nowrap">
-            Atharva Waranashiwar &nbsp;&nbsp;        
+        <div className="flex items-center bg-[#100001] px-6 h-full clip-diagonal-right border-r-2 border-[#CA0000] w-full">
+          <Link
+            to="/"
+            className="text-white text-2xl font-bold tracking-wide whitespace-nowrap font-display"
+          >
+            Atharva Waranashiwar
           </Link>
         </div>
 
         {/* Black Background - Navigation Links */}
-        <div className="flex items-center justify-right gap-8 pl-12 ml-auto mr-4">
+        <div className="flex items-center gap-8 ml-auto pr-8 animate-fade-in">
           {navItems.map((item) => (
             <Link
               key={item.name}
               to={item.path}
-              className={`relative text-white font-medium text-lg pb-1 hover:after:w-full after:block after:content-[''] after:h-[2px] after:bg-white after:transition-all after:duration-300 after:w-0 after:mx-auto ${
+              className={`relative text-white font-semibold text-[18px] pb-1 hover:-translate-y-1 hover:text-gray-200 transition-all duration-300 after:block after:content-[''] after:h-[2px] after:bg-white after:transition-all after:duration-300 after:w-0 hover:after:w-full after:mx-auto ${
                 location.pathname === item.path ? 'after:w-full' : ''
               }`}
             >
